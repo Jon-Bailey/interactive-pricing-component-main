ac9d4c76546d91d787353dba79a8ffe073bbd04e
Jon Bailey <jonbailey788@gmail.com>
2021-07-11 09:45:58 -0500

    update

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-// console log value of input slider
 const slider = document.getElementById('slider-bar');
 const price = document.getElementById('price');
+const mobilePrice = document.getElementById('mobile-price')
 
-price.textContent = '$' + slider.value;
-
-slider.oninput = function() {
-    price.textContent = '$' + this.value;
-    return price.textContent;
+//load initial price
+loadPrice = () => {
+    if (mobilePrice) {
+        mobilePrice.textContent = '$' + slider.value;
+    } else {
+        price.textContent = '$' + slider.value;
+    }
 }
 
+//update price and slider track based on slider position
+slider.oninput = function() {
+    price.textContent = '$' + Math.floor(this.value * .33) + ".00";
+    mobilePrice.textContent = '$' + Math.floor(this.value * .33) + ".00";
+    let thumbPosition = slider.value;
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%) ${thumbPosition}% , hsl(224, 65%, 95%)${thumbPosition}%)`;
+}
+
+slider.onkeydown = function() {
+    let thumbPosition = slider.value;
+    slider.style.background = `linear-gradient(90deg, hsl(174, 77%, 80%) ${thumbPosition}% , hsl(224, 65%, 95%)${thumbPosition}%)`;
+}
+
+loadPrice();
+slider.oninput();
+slider.onkeydown();
